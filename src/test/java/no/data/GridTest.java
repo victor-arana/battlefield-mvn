a1618382f933f961c989a1719c5f8f2ea5faea88
Victor Jose Arana Rodriguez <victorr@backbase.com>
2023-02-05 11:44:04 -0600

    :test: X wins by column and O wins by row

--- src/test/java/no/data/GridTest.java
+++ src/test/java/no/data/GridTest.java
@@ -15,12 +15,28 @@ public class GridTest {
     }
 
     @Test
+    public void x_should_win_by_column() {
+        var grid = new Grid();
+        grid.parseState("X_XOOXO_X");
+        grid.analizeState();
+        assertEquals(GridState.X_WINS, grid.getState());
+    }
+
+    @Test
     public void x_should_win_by_diagonal() {
         var grid = new Grid();
         grid.parseState("XOXOXOXXO");
         grid.analizeState();
         assertEquals(GridState.X_WINS,grid.getState());
 
+    }
+
+    @Test
+    public void o_should_win_by_row(){
+        var grid = new Grid();
+        grid.parseState("X__OOOX_X");
+        grid.analizeState();
+        assertEquals(GridState.O_WINS, grid.getState());
     }
 
     @Test
